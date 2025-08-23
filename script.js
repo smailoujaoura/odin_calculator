76812222cf8f7d60e9915c03d4aee46278c6baa5
Smail Oujaoura <email.oujaoura@gmail.com>
2025-08-23 09:09:07 +0100

    add a working base of the calculator

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-let RESULT_PANEL = document.querySelector(".result-panel");
-let EXPRESSION_PANEL = document.querySelector(".expression-panel");
-let BUTTONS = document.querySelector(".buttons");
+const EXPRESSION_PANEL = document.querySelector(".expression-panel");
+const RESULT_PANEL = document.querySelector(".result-panel");
+const BUTTONS = document.querySelector(".buttons");
 
-// let OPERATIONS = {
-// 	division: '/'
-// };
-
-let operation = {
+const operation = {
 	first: null,
 	second: null,
 	operator: null,
-	result: null
+};
+
+const OPERATORS = {
+	'division': '/',
+	'multiplication': '*',
+	'subtraction': '-',
+	'addition': '+',
+	'result': '='
+};
+
+function doOperation(a, b, op) {
+	a = parseInt(a);
+	b = parseInt(b);
+	if (b === 0 && op === '/') {
+		alert("Division by Zero");
+		clearInputs();
+	}
+	const ops = {
+		'+': (a, b) => a + b,
+		'-': (a, b) => a - b,
+		'*': (a, b) => a * b,
+		'/': (a, b) => a / b
+	};
+	return ops[op](a, b);
 }
 
 function makeOperation(token) {
 	if (typeof token === "number")
 	{
-		console.log("number");
 		if (operation.first === null)
 			operation.first = `${token}`;
 		else if (operation.second === null && operation.operator === null)
@@ -26,12 +44,43 @@ function makeOperation(token) {
 		else
 			operation.second += `${token}`;
 	}
-	else
+	else if (token === '-' || token === '+' || token === '/' || token === '*')
 	{
 		if (operation.first === null)
-			operation.first = '-';
-		
+			operation.first = token;
+		else if (operation.second === null)
+			operation.operator = token;
+		else
+		{
+			operation.first = doOperation(operation.first, operation.second, operation.operator);
+			operation.second = null;
+			operation.operator = token;
+		}
 	}
+	else if (token === '=') {
+		if (operation.first && operation.second)
+		{
+			operation.first = doOperation(operation.first, operation.second, operation.operator);
+			operation.second = null;
+			operation.operator = null;
+		}
+	}
+	EXPRESSION_PANEL.textContent = (operation.first ? operation.first + " " : "") + (operation.operator ? operation.operator + " " : "") + (operation.second ? operation.second + " " : "");
+	if (operation.first && operation.second) {
+		RESULT_PANEL.textContent = "= " + doOperation(operation.first, operation.second, operation.operator);
+	}
+	else if (operation.first) {
+		RESULT_PANEL.textContent = "= " + operation.first;
+	}
+	console.log(EXPRESSION_PANEL.textContent);
+}
+
+function clearInputs() {
+	EXPRESSION_PANEL.textContent = "";
+	RESULT_PANEL.textContent = "";
+	operation.first = null;
+	operation.second = null;
+	operation.operator = null;
 }
 
 function eventDelegator(e) {
@@ -40,11 +89,15 @@ function eventDelegator(e) {
 	if (targetClass.startsWith("digit-")) {
 		makeOperation(parseInt(targetClass.split('-')[1]));
 	}
-	else {
-		makeOperation(targetClass.split('-')[1]);
+	else if (targetClass.endsWith("-clear")) {
+		clearInputs();
 	}
+	else if (targetClass.endsWith("-undo")) {
 
-	console.log(e);
+	}
+	else {
+		makeOperation(OPERATORS[targetClass.split('-')[1]]);
+	}
 }
 
 function registerButtonsEvents() {
